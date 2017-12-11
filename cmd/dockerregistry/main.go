ae6d35fcfb7801db5ae1a98dfeb19f05d9a71538
Ben Parees <bparees@redhat.com>
2017-12-11 16:54:19 -0500

    move pkg/util/logs to apiserver

--- cmd/dockerregistry/main.go
+++ cmd/dockerregistry/main.go
@@ -11,7 +11,7 @@ import (
 	"time"
 
 	log "github.com/Sirupsen/logrus"
-	"k8s.io/kubernetes/pkg/util/logs"
+	"k8s.io/apiserver/pkg/util/logs"
 
 	"github.com/openshift/image-registry/pkg/cmd/dockerregistry"
 	"github.com/openshift/origin/pkg/cmd/util/serviceability"
